cfc081760ec676240dd037c53508cc8a4718d02d
SebastianoJGV <88995011+SebastianoJGV@users.noreply.github.com>
2022-04-22 11:04:41 -0700

    commit

--- bin/PythonHangMan/main.py
+++ bin/PythonHangMan/main.py
@@ -1,19 +1,23 @@
 import random
 from collections import Counter
 
-wordPool='mango apple pear banana orange'.split(' ')
+wordPool='berry berry'.split(' ')
 word=random.choice(wordPool)
-
 attemptsCounter=0
 guessedLetters=[]
+guessedWord=['_','_','_','_','_']
 
 while guessedLetters!=word:
     guess=input('Guess a letter ').lower()
     if guess in word:
         guessedLetters.append(guess)
+        counter=0
+        for guess in word:
+            index=word.index(guess)
+            guessedWord[index]=guess
         print('Correct!')
     else:
         attemptsCounter+=1
         print('Incorrect!')
-    print(Counter(guessedLetters))
+    print(guessedWord)
     
--- src/examples.py
+++ src/examples.py
@@ -1,1 +1,15 @@
-
+inp = input('enter num')
+primes=[]
+prime=False
+i = 1
+while i < int(inp):
+    for j in range(2,i):
+        if i%j==0:
+            prime=False
+            break
+        else:
+            prime=True
+    if prime:
+        primes.append(i)
+    i+=1
+print(primes)
