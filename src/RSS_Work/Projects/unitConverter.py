4a078aae73afc3f90fcb69d59e722d9d2403a8df
SebastianoJGV <88995011+SebastianoJGV@users.noreply.github.com>
2022-04-24 13:34:07 -0700

    commit

---
+++ src/RSS_Work/Projects/unitConverter.py
@@ -0,0 +1,36 @@
+weight = {
+    "kg": 1000,
+    "g": 1,
+    "mg": 0.001,
+    "lb": 453.5924,
+    "oz": 28.34952,
+    "t": 907185.07,
+}
+
+distance = {
+    "km": 1000,
+    "m": 1,
+    "cm": 0.01,
+    'mile': 1609.34,
+    'yard': 0.9144,
+    'foot': 0.3048,
+    'inch': 0.0254,
+}
+
+inp1 = input('Enter the unit to convert from, out of: \nkg, g, mg, lb, oz, t, km, m, cm, mile, yard, foot, inch \n')
+inp2 = input('Enter the unit to convert to, out of: \nkg, g, mg, lb, oz, t, km, m, cm, mile, yard, foot, inch \n')
+num = input('Enter the number to convert: \n')
+
+if inp1 in weight:
+    if inp2 in weight:
+        print(num + ' ' + inp1 + ' is ' + str(float(num) * weight[inp1] / weight[inp2]) + ' ' + inp2)
+    else:
+        print('Invalid unit')
+elif inp1 in distance:
+    if inp2 in distance:
+        print(num + ' ' + inp1 + ' is ' + str(float(num) * distance[inp1] / distance[inp2]) + ' ' + inp2)
+    else:
+        print('Invalid unit')
+else:
+    print('Invalid unit')
+
