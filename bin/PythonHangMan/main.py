095eecc2cb256da5afb03c43c44bfa8fc3236ad9
SebastianoJGV <88995011+SebastianoJGV@users.noreply.github.com>
2022-05-07 20:50:17 -0700

    Comment Changes

--- bin/PythonHangMan/main.py
+++ bin/PythonHangMan/main.py
@@ -6,17 +6,16 @@ word=random.choice(wordPool)
 attemptsCounter=0
 guessedLetters=[]
 guessedWord=['_','_','_','_','_']
-
+#cuogk
 while guessedLetters!=word:
     guess=input('Guess a letter ').lower()
     if guess in word:
         guessedLetters.append(guess)
         counter=0
         if guess in word:
-            for i in word:
-                index=word.index(guess)
-                print(index)
-                guessedWord[index]=guess
+            index=word.index(guess)
+            print(index)
+            guessedWord[index]=guess
         print('Correct!')
     else:
         attemptsCounter+=1
