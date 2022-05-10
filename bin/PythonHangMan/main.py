e5ee30dcda733efb458aeaccf3272891128d938f
SebastianoJGV <88995011+SebastianoJGV@users.noreply.github.com>
2022-05-10 11:55:28 -0700

    Comment and bugfixes

--- bin/PythonHangMan/main.py
+++ bin/PythonHangMan/main.py
@@ -1,21 +1,26 @@
+# Description : Basic Hang man game
+# Author : Sebastiano GV
+# Date Last Updated : 10th May, 2022
+# Status : In Progress
+
 import random
 from collections import Counter
 
+#wordpool contains words available, currently only two words for testing sake
 wordPool='berry berry'.split(' ')
 word=random.choice(wordPool)
 attemptsCounter=0
 guessedLetters=[]
-guessedWord=['_','_','_','_','_']
-#cuogk
+guessedWord=['_','_','_','_','_'] # we will replace the _ with the letters guessed
+
 while guessedLetters!=word:
     guess=input('Guess a letter ').lower()
-    if guess in word:
+    if guess in word: #if the letter is in the word
         guessedLetters.append(guess)
         counter=0
-        if guess in word:
-            index=word.index(guess)
-            print(index)
-            guessedWord[index]=guess
+        index=word.index(guess)
+        print(index)
+        guessedWord[index]=guess
         print('Correct!')
     else:
         attemptsCounter+=1
